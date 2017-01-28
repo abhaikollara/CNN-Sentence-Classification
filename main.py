8cee4170abf44d5ff43a6946efee51df928a2b37
abhai <abhai.dilip@gmail.com>
2017-01-28 14:49:03 +0530

    Created main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import model
+import tensorflow as tf
+import data_utils
+config = {
+    'n_epochs' : 5,
+    'kernel_sizes' : [3, 4, 5],
+    'dropout_rate' : 0.5,
+    'val_split' : 0.1,
+    'edim' : 300,
+    'n_words' : None,
+    'std_dev' : 0.1,
+    'sentence_len' : 54,
+    'n_kernels'  : 100,
+    'batch_size' : 128,
+    'paths' : ['data/rt-polarity.pos', 'data/rt-polarity.neg']
+}
+
+data, labels, w2idx = data_utils.get_data(config['paths'])
+
+config['n_words'] = len(w2idx)
+
+with tf.Session() as sess:
+    net = model.CNN(config, sess)
+    net.train(data, labels)
