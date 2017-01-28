b4c8cce2ef9d630379fbbf7fab64ed90d742bc88
abhai <abhai.dilip@gmail.com>
2017-01-28 18:15:39 +0530

    Moved config to a seperate configuration file

---
+++ configuration.py
@@ -0,0 +1,13 @@
+config = {
+    'n_epochs' : 20,
+    'kernel_sizes' : [3, 4, 5],
+    'dropout_rate' : 0.5,
+    'val_split' : 0.4,
+    'edim' : 300,
+    'n_words' : None,   #Leave as none
+    'std_dev' : 0.05,
+    'sentence_len' : 54,
+    'n_filters'  : 100,
+    'batch_size' : 50,
+    'paths' : ['data/rt-polarity.pos', 'data/rt-polarity.neg']
+}
--- main.py
+++ main.py
@@ -2,24 +2,12 @@ import os
 import model
 import tensorflow as tf
 import data_utils
-config = {
-    'n_epochs' : 20,
-    'kernel_sizes' : [3, 4, 5],
-    'dropout_rate' : 0.5,
-    'val_split' : 0.1,
-    'edim' : 300,
-    'n_words' : None,
-    'std_dev' : 0.1,
-    'sentence_len' : 54,
-    'n_filters'  : 100,
-    'batch_size' : 50,
-    'paths' : ['data/rt-polarity.pos', 'data/rt-polarity.neg']
-}
+import configuration
 
-data, labels, w2idx = data_utils.get_data(config['paths'])
+data, labels, w2idx = data_utils.get_data(configuration.config['paths'])
 
-config['n_words'] = len(w2idx)+1
+configuration.config['n_words'] = len(w2idx)+1
 
 with tf.Session() as sess:
-    net = model.CNN(config, sess)
+    net = model.CNN(configuration.config, sess)
     net.train(data, labels)
