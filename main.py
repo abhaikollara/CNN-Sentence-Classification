37d617a870a9c72cd4d8f0ab4371a31ddc56497f
abhai <abhai.dilip@gmail.com>
2017-01-28 16:54:38 +0530

    n_words is len(word2idx) + 1 to account of zero padding

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import model
 import tensorflow as tf
 import data_utils
 config = {
-    'n_epochs' : 5,
+    'n_epochs' : 20,
     'kernel_sizes' : [3, 4, 5],
     'dropout_rate' : 0.5,
     'val_split' : 0.1,
@@ -11,14 +11,14 @@ config = {
     'n_words' : None,
     'std_dev' : 0.1,
     'sentence_len' : 54,
-    'n_kernels'  : 100,
-    'batch_size' : 128,
+    'n_filters'  : 100,
+    'batch_size' : 50,
     'paths' : ['data/rt-polarity.pos', 'data/rt-polarity.neg']
 }
 
 data, labels, w2idx = data_utils.get_data(config['paths'])
 
-config['n_words'] = len(w2idx)
+config['n_words'] = len(w2idx)+1
 
 with tf.Session() as sess:
     net = model.CNN(config, sess)
